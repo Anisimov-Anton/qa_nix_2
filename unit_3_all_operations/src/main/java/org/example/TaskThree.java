a3de868cb3b6deb9aa8417dc1c1699126c2e8794
Anisimov-Anton <aanisimovv23@gmail.com>
2021-08-06 14:20:16 +0300

    unit_3_all_operations - H/w

--- unit_3_all_operations/src/main/java/org/example/TaskThree.java
+++ unit_3_all_operations/src/main/java/org/example/TaskThree.java
@@ -11,8 +11,12 @@ public class TaskThree {
 
         try {
             int x = Integer.parseInt(myObj.nextLine());
-            int resultSecondsTotal = x * 45 + (x / 2) * 5 + ((x + 1) / 2 - 1) * 15;
-            System.out.println(resultSecondsTotal / 60 + 9 + " " + resultSecondsTotal % 60);
+            if (0 < x && x < 11) {
+                int resultSecondsTotal = x * 45 + (x / 2) * 5 + ((x + 1) / 2 - 1) * 15;
+                System.out.println(resultSecondsTotal / 60 + 9 + " " + resultSecondsTotal % 60);
+            } else {
+                System.out.println("Please enter a number from 1 to 10: ");
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
